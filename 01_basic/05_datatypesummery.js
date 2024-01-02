f9dd59228700c29eb4ae214f107e40bcdcf8aff8
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2024-01-02 15:23:41 +0545

    datatype

---
+++ 01_basic/05_datatypesummery.js
@@ -0,0 +1,31 @@
+// primitive
+
+// 7 types of primitive and every primitive data is called by value
+// 1: String
+// 2: Number
+// 3: Bollean 
+// 4: null 
+// 5: undefined
+// 6: Symbol
+// 7: BigInt
+
+
+
+const score = 100
+const scoreValue = 10.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let UserEmail ;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Non-Primitive or Reference type
+
+// 1: Array
+// 2: Object
+// 3: Function
+
