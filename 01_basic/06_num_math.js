57893e4d871571364eb6399ccd855ff3dfb577b3
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2024-01-23 14:01:36 +0545

    math function

---
+++ 01_basic/06_num_math.js
@@ -0,0 +1,38 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const otherNumber = 123.865452
+// console.log(otherNumber.toPrecision(3))
+
+const hundred = 100000000
+// console.log(hundred.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++ maths +++++++++++
+
+// console.log(Math.PI)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4))
+// console.log(Math.ceil(4.4))
+// console.log(Math.floor(4.4))
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(2,5))
+// console.log(Math.min(25,45,87,2,1,0))
+// console.log(Math.max(25,45,87,2,1,0))
+
+// console.log(Math.random())
+// console.log((Math.random()*10) + 1)
+
+
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random()* (max- min + 1)) + min )
+
+
