6dbfc3c407e715fd275c5d62fe4b5534c49ca63b
RajivMahato68 <136238174+RajivMahato68@users.noreply.github.com>
2024-01-30 21:40:06 +0545

    forin loop and map

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+//    console.log(`${key} for shortcut is for${myObject[key]}`)
+}
+
+const programmming = ["js", "rb","py", "java"]
+
+for (const key in programmming) {
+//   console.log(`${key} for programming language ${programmming[key]}`)
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('NP', "Nepal")
+// map.set('USA', "United State of America")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key)
+// }
+
